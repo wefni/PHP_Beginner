5bffa932ef5029faca58a15493689dba00c415c4
wefni <halaszdavid10@gmail.com>
2020-09-15 19:40:55 +0200

    Sima skeleton

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  require "header.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+
+<?php
+  require "footer.php";
+?>
